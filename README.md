44cc333c9e8bd28d820f93b01ec67cf439f208ef
THXBurke <rachelwheels@gmail.com>
2016-04-12 12:04:29 -0700

    client server

--- README.md
+++ README.md
@@ -10,8 +10,11 @@
 ##Description
 Create a simple tcp logging server. This server should receive tcp requests and save the request into a file. Each request should be saved into it's own file and you'll have to find something unique to name them. You can use a UUID library or the current time or any other means of having unique strings that you can think of.
 
- 
+
 ##Rubric
 unique name: 4pts
 logging into file: 4pts
 testing: 2pts
+
+
+I found a helpful example at http://www.hacksparrow.com/tcp-socket-programming-in-node-js.html
--- tcp_server.js
+++ tcp_server.js
@@ -2,6 +2,7 @@
 
 var net = require('net');
 var fs = require('fs');
+var request = '';
 var HOST = '127.0.0.1';
 var PORT = 3000;
 
@@ -9,18 +10,18 @@ net.createServer(function(socket) {
 
   console.log('CONNECTION: ' + socket.remoteAddress + ':' + socket.remotePort);
 
-  socket.on('data', function(data);
+  socket.on('data', function(data) {
 
     console.log('DATA ' + socket.remoteAddress + ': ' + data);
-    socket.write('You said Burke ' + data);
+    socket.write('You said Burke ' + data + "");
 
   });
 
   socket.on('close', function(data) {
-  console.log('Closed: ' + socket.remoteAddress + '' + sock.remotePort);
+  console.log('Closed: ' + socket.remoteAddress + '' + socket.remotePort);
 
   });
 
-}); .listen(PORT, HOST);
+}).listen(PORT, HOST);
 
 console.log('Server listening on ' + HOST + ':' + PORT);
