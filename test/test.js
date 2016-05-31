e05d715948c53954f008d6dd2a6c6bc213b68489
THXBurke <rachelwheels@gmail.com>
2016-05-30 17:26:17 -0700

    rewrote server

--- test/test.js
+++ test/test.js
@@ -1,24 +1,23 @@
-const expect = require('chai').expect;
-const testServer = require(__dirname + '/../lib/server.js');
-const fs = require('fs');
-const net = require('net');
+const chai = require('chai');
+const expect = chai.expect;
+const tcp = require(__dirname + '/../lib/server.js');
+const chaiHttp = require('chai-http');
 
-describe('server', () => {
-  it('should create a server on localhost 3000', (done) => {
-   expect(testServer.address().port).to.eql(3000);
-   done();
+chai.use(chaiHttp);
+
+describe('Testing tcp server', () => {
+  after(() => {
+    tcp.server.close();
   });
-
-  it('should write to new file on GET request', (done) => {
-    var directoryLength = fs.readdirSync(__dirname + '/../newData').length;
-    var client = net.connect( { 'port': 3000 }, () => {} );
-    client.emit('end');
-    client.on('end', () => {
-      fs.readdir(__dirname + '/../newData', (err, array) => {
-        if (err) return console.log(err);
-        expect(directoryLength + 1).to.eql(array.length);
+  it('should have a response from the server', (done) => {
+    chai.request('http:localhost:8000')
+      .get('/')
+      .then((res) => {
+        expect(res).to.have.status(200);
         done();
+      })
+      .catch((err) => {
+        throw err;
       });
-    });
   });
 });
--- test/writeLogTest.js
+++
@@ -1,8 +0,0 @@
-const expect = require('chai').expect;
-const writeLogTest = require(__dirname + '/..lib/writeLogTest');
-
-describe('test writeLog file', () => {
-  it('writeLog should be a working function', () => {
-    expect(typeof writeLog).to.eql('function');
-  });
-});
